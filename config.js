deb4b1ec32732d6366e420d80a200e7958858fa5
Vladislav Muksinov <vmuksinov_bbt@medable.com>
2021-11-29 12:30:31 +0300

    > Added token service

--- config.js
+++ config.js
@@ -1,18 +1,26 @@
 const environments = {
-    staging:{
-        httpPort:3000,
-        httpsPort:3001,
-        envName:"staging"
-    },
-    production:{
-        httpPort:5000,
-        httpsPort:5001,
-        envName:"production"
-    }
-}
+  staging: {
+    httpPort: 3000,
+    httpsPort: 3001,
+    envName: "staging",
+    secret: "somesecret",
+  },
+  production: {
+    httpPort: 5000,
+    httpsPort: 5001,
+    envName: "production",
+    secret: "somesecret",
+  },
+};
 
-const currentEnv = (typeof process.env.NODE_ENV == "string") ? process.env.NODE_ENV.toLowerCase() : "";
+const currentEnv =
+  typeof process.env.NODE_ENV == "string"
+    ? process.env.NODE_ENV.toLowerCase()
+    : "";
 
-const exportEnv = (typeof environments[currentEnv] == "object") ? environments[currentEnv] : environments.staging;
+const exportEnv =
+  typeof environments[currentEnv] == "object"
+    ? environments[currentEnv]
+    : environments.staging;
 
 module.exports = exportEnv;
--- index.js
+++ index.js
@@ -1,73 +1,82 @@
 //dependencies
-const http = require("http")
-const https = require("https")
-const fs = require("fs")
-const url = require("url")
-const StringDecoder = require("string_decoder").StringDecoder
-const config = require("./config")
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
+const config = require("./config");
+const handlers = require("./lib/handlers");
+const helpers = require("./lib/helpers");
 
 const httpsOptions = {
-    "key": fs.readFileSync("./https/key.pem"),
-    "cert": fs.readFileSync("./https/cert.pem"),
-}
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem"),
+};
 
-const httpServer = http.createServer((req, res)=>{
-    unifiedServer(req, res)
-}).listen(config.httpPort, ()=>console.log("HTTP server is running on port " + config.httpPort))
+const _data = require("./lib/data");
+const { Hmac } = require("crypto");
 
-const httpsServer = https.createServer(httpsOptions, (req, res)=>{
-    unifiedServer(req, res)
-}).listen(config.httpsPort, ()=>console.log("HTTPS server is running on port " + config.httpsPort))
+http
+  .createServer((req, res) => {
+    unifiedServer(req, res);
+  })
+  .listen(config.httpPort, () =>
+    console.log("HTTP server is running on port " + config.httpPort)
+  );
 
-function unifiedServer(req, res){
-    //parse url
-    const parsedUrl = url.parse(req.url, true)
-    const path = parsedUrl.pathname
-    const trimmedPath = path.replace(/^\/|\/+$/g,"")
-    const queryStringObg = parsedUrl.query
-    //get method
-    const method = req.method.toLowerCase()
-    //get headers
-    const headers = req.method.headers
-    const decoder = new StringDecoder("utf-8")
-    let buffer = ""
+https
+  .createServer(httpsOptions, (req, res) => {
+    unifiedServer(req, res);
+  })
+  .listen(config.httpsPort, () =>
+    console.log("HTTPS server is running on port " + config.httpsPort)
+  );
 
-    req.on("data",(chunk)=>{
-        buffer += decoder.write(chunk)
-    })
+function unifiedServer(req, res) {
+  //parse url
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/|\/+$/g, "");
+  const queryStringObj = parsedUrl.query;
+  //get method
+  const method = req.method.toLowerCase();
+  //get headers
+  const headers = req.headers;
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
 
-    req.on("end",()=>{
-        buffer += decoder.end()
-        const chosenHandler = (typeof router[trimmedPath] !== "undefined") ? router[trimmedPath] : handlers.notFound
-        const data = {
-            trimmedPath,
-            queryStringObg,
-            method,
-            headers,
-            payload:buffer
-        }
-        chosenHandler(data, (statusCode, payload)=>{
-            statusCode = (typeof statusCode == "number") ? statusCode : 200
-            payload = (typeof payload == "object") ? payload : {}
-            const payloadString = JSON.stringify(payload)
-            //return response
-            res.setHeader("Content-Type","application/json")
-            res.writeHead(statusCode)
-            res.end(payloadString)
-            console.log(trimmedPath, statusCode);
-        })
-    })
-}
+  req.on("data", (chunk) => {
+    buffer += decoder.write(chunk);
+  });
 
-const handlers = {
-    ping(data, callback){
-        callback(200)
-    },
-    notFound(data, callback){
-        callback(404)
-    }
+  req.on("end", () => {
+    buffer += decoder.end();
+    const chosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+    const data = {
+      trimmedPath,
+      queryStringObj,
+      method,
+      headers,
+      payload: helpers.convertToObject(buffer),
+    };
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
+      payload = typeof payload == "object" ? payload : {};
+      const payloadString = JSON.stringify(payload);
+      //return response
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      console.log(trimmedPath, statusCode);
+    });
+  });
 }
 
 const router = {
-    "ping":handlers.ping
-}
+  ping: handlers.ping,
+  users: handlers.users,
+  tokens: handlers.tokens,
+};
