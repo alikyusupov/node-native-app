038955fb4b9921c7dc03038f0d9aeed9bc86f3e2
Vladislav Muksinov <vmuksinov_bbt@medable.com>
2022-04-01 10:25:57 +0300

    add delete session

--- public/app.js
+++ public/app.js
@@ -82,6 +82,38 @@ app.client.request = function (headers, path, method, queryStringObject, payload
 
 };
 
+// Bind the logout button
+app.bindLogoutButton = function () {
+  document.getElementById("logoutButton").addEventListener("click", function (e) {
+
+    // Stop it from redirecting anywhere
+    e.preventDefault();
+
+    // Log the user out
+    app.logUserOut();
+
+  });
+};
+
+// Log the user out then redirect them
+app.logUserOut = function () {
+  // Get the current token id
+  var tokenId = typeof (app.config.sessionToken.id) == 'string' ? app.config.sessionToken.id : false;
+
+  // Send the current token to the tokens endpoint to delete it
+  var queryStringObject = {
+    'id': tokenId
+  };
+  app.client.request(undefined, 'api/tokens', 'DELETE', queryStringObject, undefined, function (statusCode, responsePayload) {
+    // Set the app.config token as false
+    app.setSessionToken(false);
+
+    // Send the user to the logged out page
+    window.location = '/session/deleted';
+
+  });
+};
+
 // Bind the forms
 app.bindForms = function () {
   if (document.querySelector("form")) {
@@ -161,9 +193,8 @@ app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
   }
   // If login was successful, set the token in localstorage and redirect the user
   if (formId == 'sessionCreate') {
-    console.log('Redirecting...');
+    console.log('responsePayload');
     app.setSessionToken(responsePayload);
-    console.log('Redirecting...');
     window.location = '/checks/all';
   }
 };
@@ -260,6 +291,9 @@ app.init = function () {
 
   // Bind all form submissions
   app.bindForms();
+
+  // Bind logout logout button
+  app.bindLogoutButton();
 
   // Get the token from localstorage
   app.getSessionToken();
